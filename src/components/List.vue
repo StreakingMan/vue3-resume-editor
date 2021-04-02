f386e2a2562705aeee995e5c40dfc94fb7d72be5
streakingman <z_max_y@163.com>
2021-04-02 17:43:04 +0800

    chore: virtualGroup drag demo

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,32 +1,20 @@
 <template>
     <div class="list ma-6">
         <div v-for="(item, index) in items" :key="item.id">
-            <template v-if="calcVirtualGroups(items[index - 1], item).length">
-                <div
-                    v-for="(group, idx) in calcVirtualGroups(
-                        items[index - 1],
-                        item
-                    )"
-                    :key="group"
-                    class="group"
-                    :style="{
-                        marginLeft:
-                            item.groupName.split('.').length * 24 -
-                            (calcVirtualGroups(items[index - 1], item).length -
-                                idx) *
-                                24 +
-                            'px',
-                    }"
-                >
-                    {{ group }}
-                </div>
-            </template>
+            <ListVirtualGroup
+                :pre-item="items[index - 1]"
+                :cur-item="item"
+            ></ListVirtualGroup>
 
             <div
                 class="item"
                 :style="{
                     marginLeft: item.groupName.split('.').length * 24 + 'px',
                 }"
+                draggable="true"
+                @dragstart="onDragstart($event, item, index)"
+                @dragover="onDragover($event)"
+                @drop="onDrop($event, item, index)"
             >
                 {{ item.id }}:{{ item.groupName }}
             </div>
@@ -37,24 +25,46 @@
 <script lang="ts">
 import { defineComponent, inject, ref } from 'vue';
 import { Item } from '../interface/Item';
-import { getGroupsByGroupNameDiff } from '../algorithms/group';
+import ListVirtualGroup from './ListVirtualGroup.vue';
 
 export default defineComponent({
     name: 'List',
+    components: { ListVirtualGroup },
     setup() {
         const items = ref<Item[]>(inject('items', []));
 
-        // TODO 包装到computed中
-        const calcVirtualGroups = (preItem: Item, curItem: Item) => {
-            return getGroupsByGroupNameDiff(
-                preItem?.groupName || '',
-                curItem.groupName
+        const onDragstart = (evt: DragEvent, item: Item, dragIndex: number) => {
+            evt.dataTransfer.setData(
+                'text/plain',
+                JSON.stringify({ item, dragIndex })
             );
+        };
+
+        const onDrop = (evt: DragEvent, item: Item, dropIndex: number) => {
+            evt.preventDefault();
+            try {
+                const { dragIndex } = JSON.parse(
+                    evt.dataTransfer.getData('text')
+                );
+                [items.value[dragIndex], items.value[dropIndex]] = [
+                    items.value[dropIndex],
+                    items.value[dragIndex],
+                ];
+            } catch (e) {
+                console.log(e);
+                return;
+            }
+        };
+
+        const onDragover = (evt: DragEvent) => {
+            evt.preventDefault();
         };
 
         return {
             items,
-            calcVirtualGroups,
+            onDragstart,
+            onDragover,
+            onDrop,
         };
     },
 });
@@ -63,16 +73,12 @@ export default defineComponent({
 <style lang="scss" scoped>
 .list {
     width: 300px;
-    .item,
-    .group {
+    .item {
         margin: 4px;
         padding: 4px;
         border-radius: 2px;
         border: 1px solid grey;
         user-select: none;
-    }
-    .group {
-        background-color: lightblue;
     }
 }
 </style>
