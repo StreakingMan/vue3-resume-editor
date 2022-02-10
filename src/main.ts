679785e9dfdc2acc7b855b17f00ed7c4afef72f1
streakingman <z_max_y@163.com>
2022-02-10 16:10:07 +0800

    chore: add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './styles/index.scss';
-import '@mdi/font/css/materialdesignicons.css';
-import 'animate.css/animate.min.css';
+import vuetify from './plugins/vuetify';
+import { loadFonts } from './plugins/webfontloader';
 import { i18n } from './i18n';
 
-createApp(App).use(i18n).mount('#app');
+loadFonts().then();
+
+createApp(App).use(vuetify).use(i18n).mount('#app');
