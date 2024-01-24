1710df4bd60a48341517f595b627c14c669d538e
streakingman <z_max_y@163.com>
2024-01-24 19:41:19 +0800

    feat: 模版预览卡片渲染元素组件

--- src/components/templates/TemplateList.vue
+++ src/components/templates/TemplateList.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import template1 from './resume-template-1.json';
+import template2 from './resume-template-2.json';
+import TemplatePreviewCard from './TemplatePreviewCard.vue';
+
+const templateList = [template1, template2];
+</script>
+
 <template>
     <div
         class="d-flex align-center justify-start w-100 h-100 overflow-x-auto px-10"
@@ -12,18 +20,3 @@
         >
     </div>
 </template>
-
-<script lang="ts">
-import template1 from './resume-template-1.json';
-import template2 from './resume-template-2.json';
-import { defineComponent } from 'vue';
-import TemplatePreviewCard from './TemplatePreviewCard.vue';
-
-export default defineComponent({
-    name: 'TemplateList',
-    components: { TemplatePreviewCard },
-    data: () => ({
-        templateList: [template1, template2],
-    }),
-});
-</script>
--- src/components/templates/TemplatePreviewCard.vue
+++ src/components/templates/TemplatePreviewCard.vue
@@ -1,3 +1,54 @@
+<script setup lang="ts">
+import { computed, provide, ref } from 'vue';
+import { Paper as PaperClass, paperInjectionKey } from '@/classes/Paper';
+import { usePaper, useRuntime } from '@/composables/useApp';
+import Paper from '@/components/core/Paper.vue';
+
+const props = defineProps<{
+    template: any;
+}>();
+
+const matrix = ref<number[][]>([]);
+const handleMouseMove = (e: MouseEvent) => {
+    const { offsetX, offsetY, target } = e;
+    const x = offsetX - (target as HTMLElement).offsetWidth / 2;
+    const y = offsetY - (target as HTMLElement).offsetHeight / 2;
+    matrix.value = [
+        [1, 0, 0, x * 0.000005],
+        [0, 1, 0, y * 0.000005],
+        [0, 0, 1, 1],
+        [0, 0, 0, 1],
+    ];
+};
+const handleMouseLeave = () => {
+    matrix.value = [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
+    ];
+};
+
+const transform = computed(() => {
+    return `matrix3d(${matrix.value.toString()})`;
+});
+
+const paperInstance = new PaperClass({});
+paperInstance.loadData(props.template);
+provide(paperInjectionKey, paperInstance);
+
+const runtime = useRuntime();
+const realPaper = usePaper();
+
+const onClick = () => {
+    const res = window.confirm('模板将覆盖当前页面，确认使用？');
+    if (res) {
+        realPaper.loadData(props.template);
+        runtime.bottomDrawer = false;
+    }
+};
+</script>
+
 <template>
     <div
         :style="{
@@ -7,88 +58,12 @@
         @mousemove="handleMouseMove"
         @mouseleave="handleMouseLeave"
     >
-        <div class="template-preview--card" :style="{ backgroundColor: paper.background }">
-            <div
-                v-for="m in paper.materialList.filter((m) => m.componentName !== 'MDivider')"
-                :key="m.id"
-                :style="{
-                    background: m?.config?.backgroundColor || 'rgba(128,128,128,0.3)',
-                    borderRadius: (m?.config?.borderRadius || 0) + 'px',
-                    position: 'absolute',
-                    left: m.x + 'px',
-                    top: m.y + 'px',
-                    width: m.w + 'px',
-                    height: m.h + 'px',
-                }"
-            ></div>
+        <div class="template-preview--card" :style="{ backgroundColor: paperInstance.background }">
+            <Paper />
         </div>
         <v-btn class="btn" color="primary" elevation="8" @click="onClick"> 使用模板 </v-btn>
     </div>
 </template>
-
-<script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
-import { Paper } from '../../classes/Paper';
-import { usePaper, useRuntime } from '../../composables/useApp';
-
-export default defineComponent({
-    name: 'TemplatePreviewCard',
-    props: {
-        template: {
-            type: Object,
-            required: true,
-        },
-    },
-    setup(props) {
-        const matrix = ref<number[][]>([]);
-        const handleMouseMove = (e: MouseEvent) => {
-            const { offsetX, offsetY, target } = e;
-            const x = offsetX - (target as HTMLElement).offsetWidth / 2;
-            const y = offsetY - (target as HTMLElement).offsetHeight / 2;
-            matrix.value = [
-                [1, 0, 0, x * 0.000005],
-                [0, 1, 0, y * 0.000005],
-                [0, 0, 1, 1],
-                [0, 0, 0, 1],
-            ];
-        };
-        const handleMouseLeave = () => {
-            matrix.value = [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 1, 0],
-                [0, 0, 0, 1],
-            ];
-        };
-
-        const transform = computed(() => {
-            return `matrix3d(${matrix.value.toString()})`;
-        });
-
-        const paper = new Paper({});
-        paper.loadData(props.template as any);
-
-        const runtime = useRuntime();
-        const realPaper = usePaper();
-
-        const onClick = () => {
-            const res = window.confirm('模板将覆盖当前页面，确认使用？');
-            if (res) {
-                realPaper.loadData(props.template as any);
-                runtime.bottomDrawer = false;
-            }
-        };
-
-        return {
-            onClick,
-            paper,
-            transform,
-            handleMouseMove,
-            handleMouseLeave,
-        };
-    },
-});
-</script>
 
 <style scoped lang="scss">
 .btn {
@@ -99,6 +74,7 @@ export default defineComponent({
     transition: 0.3s;
     opacity: 0;
 }
+
 .template-preview {
     &--card {
         width: 793px;
@@ -114,10 +90,12 @@ export default defineComponent({
 
     &--wrapper {
         position: relative;
+
         &:hover {
             .btn {
                 opacity: 0.7;
             }
+
             .template-preview--card {
                 transform: translateY(-100px);
             }
