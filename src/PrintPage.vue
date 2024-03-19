c39f1bc60f29388aa2f8098f0a02203a01c51bac
streakingman <z_max_y@163.com>
2024-03-19 15:09:57 +0800

    fix: 打印预览页使用动态时间标题

--- src/PrintPage.vue
+++ src/PrintPage.vue
@@ -2,10 +2,13 @@
 import Paper from '@/components/core/Paper.vue';
 import { onMounted } from 'vue';
 
-import { useWindowScroll, useWindowSize } from '@vueuse/core';
+import { useWindowScroll, useWindowSize, useTitle, useDateFormat, useNow } from '@vueuse/core';
 import { linear } from '@/utils/timeFunction';
 import { createAndProvideReactivePaper, usePaperMode } from '@/composables/usePaper';
 import { PaperMode } from '@/classes/Paper';
+
+// 更改标题格式为：resume-2021/8/1-12:00:00
+useTitle().value = `resume-${useDateFormat(useNow(), 'YYYY/M/D-H:m:s').value}`;
 
 // Paper实例
 const paperInstance = createAndProvideReactivePaper();
@@ -27,7 +30,7 @@ onMounted(async () => {
             window.print();
             return;
         }
-        const duration = (scrollHeight / 1000) * 1000;
+        const duration = (scrollHeight / 1000) * 2000;
         linear({
             start: 0,
             end: scrollHeight,
